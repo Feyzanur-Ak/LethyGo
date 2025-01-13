7bf4007c208db9d758031717b93b878264469b69
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2025-01-13 20:46:57 +0300

    DropDown Menu düzenleme

--- LethyGo/src/layout/DropDownMenu.jsx
+++ LethyGo/src/layout/DropDownMenu.jsx
@@ -18,10 +18,9 @@ const DropDownMenu = () => {
         <ChevronDown className="w-4 h-4 relative top-1"/>
       </button>
       {isOpen && (
-      <div className="absolute mt-3 py-4 bg-white shadow-md rounded-lg w-[396px] h-[272px] z-50 ">
-        <div className="cursor-pointer" >
-            <div className="flex flex-row justify-around gap-8 px-4 "> 
-                <div className="flex flex-col  gap-2">
+      <div className="absolute mt-3 py-4 bg-neutral-50 shadow-md  w-[496px] h-[272px] z-50 cursor-pointer ">
+            <div className="flex flex-row items-left gap-12    "> 
+                <div className="flex flex-col ml-5  gap-2 text-left w-[30%]">
                     <h4 className="text-slate-800 font-semibold">Kadın</h4>
                     <ul className="flex flex-col gap-4 text-neutral-500 py-6">
                         <li > <a href="#" ></a>Tshirt {/* Linkler güncellenecek sayfalar oluşturuldukça*/}
@@ -29,7 +28,7 @@ const DropDownMenu = () => {
                         <li><a href="#" ></a>Shoes </li>
                     </ul>
                 </div>
-                <div className="flex flex-col gap-1">
+                <div className="flex flex-col  gap-2 text-left w-[30%] ">
                     <h4 className="text-slate-800 font-semibold">Erkek</h4>
                     <ul className="flex flex-col gap-4 text-neutral-500 py-6">
                         <li> <a href="#"></a>Tshirt</li>
@@ -37,7 +36,6 @@ const DropDownMenu = () => {
                     </ul>
                 </div>
             </div>
-        </div>
       </div>
       )}
     </div>
